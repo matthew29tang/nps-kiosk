30621073df2c45b98fc6a23a01eabece05962ca5
matthew29tang <matthew29tang@gmail.com>
2019-06-06 19:37:35 -0700

    Update About

---
+++ backend-server/server.js
@@ -0,0 +1,79 @@
+var port = process.env.PORT || 8888; //sets local server port to 8888
+var express = require('express'); // Express web server framework
+var request = require('request');
+
+var token = 'xLzbrpcuMwDXar76GlDLcIZrzRu7Cv1KJHz7yZZX';
+
+var redirect_uri = 'http://localhost:8888'; // Your redirect uri in case you are using apis
+var app = express();
+var data = {};
+data.parks = {};
+var allParks = [];
+
+console.log("Starting up server... Making requests");
+function objToQueryString(obj){
+  const keyValuePairs = [];
+  for (const key in obj) {
+    keyValuePairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+  }
+  return keyValuePairs.join('&');
+}
+
+function formatURL(params, req) {
+  params.api_key = token;
+  return `https://developer.nps.gov/api/v1/${req}?` + objToQueryString(params);
+}
+
+function getRequest(callback, url, stateCode){
+  var options = {
+    url: url,
+    method: 'GET'
+  };
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log("Received " + stateCode);
+      callback(formatParks(body, stateCode), stateCode);
+    }
+  });
+}
+
+function formatParks(parks, stateCode){
+  return {data: JSON.parse(parks).data.map(park => {
+    var p = {
+      description: park.description,
+      parkCode: park.parkCode,
+      fullName: park.fullName,
+      stateCode: stateCode
+    }
+    var search = {
+      label: park.fullName + ` (${stateCode})`,
+      value: park.parkCode
+    }
+    allParks.push(search);
+    return p;
+  })}
+}
+const states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 
+'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
+'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 
+'WA', 'WV', 'WI', 'WY'];
+
+states.map(state => {
+  getRequest((parks, state) => data.parks[state] = parks, formatURL({stateCode: state}, 'parks'), state);
+})
+
+app.get('/parks', function(req, res) {
+  console.log("Displaying parks");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send(data.parks[req.query.stateCode]);
+});
+
+app.get('/allParks', function(req, res) {
+  console.log("Displaying allParks");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send(allParks);
+});
+
+app.listen(port, function() {}); //starts the server, alternatively you can use app.listen(port)
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,25 +6,36 @@ class About extends React.Component {
 
   render() {
     return (
-        <div className="About">
-          <h1>About</h1>
-          <Divider />
-          <br/>
-          This project was created for the National Park MindSumo project. More details can be found 
-          <a href="https://www.mindsumo.com/contests/national-park-api"> here.</a>
-          <br/><br/>
-          Users search for a list of parks by state and can browse the list for a park of their choice or search by park name.
-          Upon clicking that park, there are several tabs listed so the user can access information about that park,
-          nearby visitor centers and campgrounds, articles/events/news, and educational resources about the park.
+      <div className="About">
+        <h1>About</h1>
+        <Divider />
+        <br />
+        <div align="left" word-wrap="break-word">
+          This project was created for the National Park MindSumo project. More details can be found <a href='https://www.mindsumo.com/contests/national-park-api'> here</a>. 
+          The goal was to provide a clean, sleek interface for the user to access information about national parks.
+          <br />
+          <h2>Features</h2>
+          <ul>
+            <li>Search by state (with autocomplete, flil list of states)</li>
+            <li>Search by park name (with autocomplete, flil list of parks) [Uses custom backend server]</li>
+            <li>Park information</li>
+            <li>Nearby visitor centers and campgrounds</li>
+            <li>Articles/events/news</li>
+            <li>Educational resources about the park.</li>
+          </ul>
+          <h2>Technologies used</h2>
+          <ul>
+            <li>Frontend: ReactJS</li>
+              <li style={{marginLeft:"2em"}}>Theme: Material UI</li>
+              <li style={{marginLeft:"2em"}}>Deployment: Github pages</li>
+            <li>Backend: NodeJS</li>
+              <li style={{marginLeft:"2em"}}>Deployment: Heroku (nps-kiosk-server.herokuapp.com)</li>
+              <li style={{marginLeft:"2em"}}>Endpoints: See <a href="https://github.com/matthew29tang/nps-kiosk/blob/master/README.md"> this link</a> for documentation</li>
+          </ul>
+          <br />
           I created a backend server which provides a smaller JSON payload to reduce park listing delays.
-          <br/><br/>
-          The goal was to provide a clean, sleek interface for the user to access such information. Material UI was used for the theme.
-          <br/><br/>
-          Some challenges included making the API requests. 
-          The park listing by state loads slowly on this site due to the large size of the JSON response payload from the API.
-          The endpoints are configured such that it is impossible to only request certain attributes about each park (there are default parameters which cannot be removed),
-          making the delay time very noticeable. To mitigate this issue, I created a custom backend server for park listing by state.
         </div>
+      </div>
     );
   }
 }
